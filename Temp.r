8630ce3ea768cf1b74f1779a105dce873a4828f5
MRAG <mrag@mrags-imac.nmfs.local>
2015-08-12 13:00:12 -0700

    Cleaned the code, added explanations for the variuous parts of the .r file

--- Temp.r
+++ Temp.r
@@ -1,5 +1,6 @@
 list.of.packages <- c("dplyr","plyr","lubridate","descr",
                       "pbapply","seas")
+
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
